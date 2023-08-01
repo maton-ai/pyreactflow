maton-ai/pyreactflow

commit d78439bf4dfaa5555398c3217217b6ed281b7753

Revised variable names and added more comments Made variable names more descriptive. Figured out I could use triple quotes to block raw text so replaced the python read in strings with raw html. Added more comments in both documents trying to explain what its doing. Added print satement if output written to file.

pyflowchart/__main__.py
@@ -12,7 +12,11 @@ import chardet
 
 from pyflowchart.flowchart import Flowchart
 
-from pyflowchart.output_html import html_part_1,html_part_2,html_part_3
+from pyflowchart.output_html import html_part_before_title,html_part_after_title_before_code_block,html_part_after_code_block_remaining_html
+
+
+
+
 import os
 
 def detect_decode(file_content: bytes) -> str:
@@ -64,16 +68,26 @@ def main(code_file, field, inner, output, simplify, conds_align):
                                     simplify=simplify,
                                     conds_align=conds_align)
     print(flowchart.flowchart())
-    #if output file was specified, write output html
+    #If output file was specified, 
+    #Write output html format
+    #html blocks stored as variables in output_html.py
+    #html_part_before_title is header before <title>, 
+    #If field specified use that as title. If not, use output filename as title.
+    #Output remainder of header up to the code block (html_part_after_title_before_code_block)
+    #Output the flowchart.flowchart() into the code block section of the html
+    #Output the remainder of the html. (html_part_after_code_block_remaining_html)
+    #End code block. Add couple buttons. Add output canvas.
     if bool(output):
         with open(output, 'w') as f:
-            f.write(html_part_1)
-            f.write(os.path.basename(f.name))
-            f.write(html_part_2)
+            f.write(html_part_before_title)
+            if bool(field):
+                f.write(field)
+            else:
+                f.write(os.path.basename(f.name))
+            f.write(html_part_after_title_before_code_block)
             f.write(flowchart.flowchart())
-            f.write(html_part_3)
-        
-
+            f.write(html_part_after_code_block_remaining_html)
+            print(f"Saved HTML to {output}")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Python code to flowchart.')

pyflowchart/output_html.py
@@ -5,7 +5,136 @@ This is straight up https://github.com/adrai/flowchart.js/blob/master/example/in
 with minor color and size changes.
 Use of this source code is governed by a MIT
 license that can be found in https://github.com/adrai/flowchart.js/blob/master/license
+To keep it simple this is a sandwich of html.
+Top header
+<insert title>
+Rest header into body html until you get to code block
+<insert code block>
+Remainder body of html closing up the code block, adding couple buttons, and canvas generated by code block.
 """
-html_part_1='<!DOCTYPE html>\n<html lang="en">\n    <head>\n        <meta charset="utf-8">\n        <title>'
-html_part_2='</title>\n        <style type="text/css">\n          .end-element { fill : #FFCCFF; }\n        </style>\n        <script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.3.0/raphael.min.js"></script>\n        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowchart/1.17.1/flowchart.min.js"></script>\n        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.1/js/bootstrap.bundle.min.js"></script>\n        <!-- <script src="../release/flowchart.min.js"></script> -->\n        <script>\n\n            window.onload = function () {\n                var btn = document.getElementById("run"),\n                    cd = document.getElementById("code"),\n                    chart;\n\n                (btn.onclick = function () {\n                    var code = cd.value;\n\n                    if (chart) {\n                      chart.clean();\n                    }\n\n                    chart = flowchart.parse(code);\n                    chart.drawSVG(\'canvas\', {\n                      \'x\': 0,\n                      \'y\': 0,\n                      \'line-width\': 3,\n                      //\'maxWidth\': 15,//ensures the flowcharts fits within a certain width\n                      \'line-length\': 50,\n                      \'text-margin\': 10,\n                      \'font-size\': 14,\n                      \'font\': \'normal\',\n                      \'font-family\': \'Helvetica\',\n                      \'font-weight\': \'normal\',\n                      \'font-color\': \'black\',\n                      \'line-color\': \'black\',\n                      \'element-color\': \'black\',\n                      \'fill\': \'white\',\n                      \'yes-text\': \'yes\',\n                      \'no-text\': \'no\',\n                      \'arrow-end\': \'block\',\n                      \'scale\': 1,\n                      \'symbols\': {\n                        \'start\': {\n\t\t\t\t\t\t  \'font-size\': 14,\n                          \'font-color\': \'yellow\',\n                          \'element-color\': \'blue\',\n                          \'fill\': \'green\',\n\t\t\t\t\t\t  \'class\': \'start-element\'\n                        },\n                        \'inputoutput\': {\n                          \'font-color\': \'black\',\n                          \'element-color\': \'black\',\n                          \'fill\': \'bisque\'\n                        },\n                        \'operation\': {\n                          \'font-color\': \'black\',\n                          \'element-color\': \'black\',\n                          \'fill\': \'linen\'\n                        },\n                        \'subroutine\': {\n                          \'font-color\': \'black\',\n                          \'element-color\': \'blue\',\n                          \'fill\': \'lightgreen\'\n                        },\n                        \'condition\': {\n                          \'font-color\': \'red\',\n\t\t\t\t\t\t  \'element-color\': \'black\',\n                          \'fill\': \'yellow\'\n                        },\n                        \'end\':{\n\t\t\t\t\t\t  \'font-size\': 20,\n                          \'class\': \'end-element\'\n                        }\n                      },\n                      \'flowstate\' : {\n                        //\'past\' : { \'fill\' : \'#CCCCCC\', \'font-size\' : 12},\n                        //\'current\' : {\'fill\' : \'yellow\', \'font-color\' : \'red\', \'font-weight\' : \'bold\'},\n                        //\'future\' : { \'fill\' : \'#FFFF99\'},\n                        \'request\' : { \'fill\' : \'blue\'},\n                        \'invalid\': {\'fill\' : \'#444444\'},\n                        \'approved\' : { \'fill\' : \'#58C4A3\', \'font-size\' : 12, \'yes-text\' : \'APPROVED\', \'no-text\' : \'n/a\' },\n                        \'rejected\' : { \'fill\' : \'#C45879\', \'font-size\' : 12, \'yes-text\' : \'n/a\', \'no-text\' : \'REJECTED\' }\n                      }\n                    });\n\n                })();\n\n            };\n            function myFunction(event, node) {\n              console.log("You just clicked this node:", node);\n            }\n            \n        </script>\n\t\t<script>\n\t\t\tfunction HelpText() {\n\t\t\t  var x = document.getElementById("HelpTextBlock");\n\t\t\t  if (x.style.display === "none") {\n\t\t\t\tx.style.display = "block";\n\t\t\t  } else {\n\t\t\t\tx.style.display = "none";\n\t\t\t  }\n\t\t\t}\n\t\t</script>\n    </head>\n    <body>\n        <div><textarea id="code" style="width: 100%;" rows="11">'
-html_part_3=')</textarea></div>\n        <div><button id="run" type="button">Run</button> <button onclick="HelpText()">Format Help</button></div>\n        \n\t\t<div id="HelpTextBlock" style="display:none">Conditions can also be redirected like cond(yes, bottom) or cond(yes, right)\n... and the other symbols too... like sub1(right)<br/>\nYou can also tweak the diagram.drawSVG(\'diagram\', {}); script section in this file for more changes to how it looks.<br/>\nThis is based on <a href="https://github.com/adrai/flowchart.js">flowchart.js on github</a> and <a href="http://flowchart.js.org">http://flowchart.js.org</a> more documentation can be found over there.\n</div>\n\n        <div id="canvas"></div>\n    </body>\n</html>'
+
+html_part_before_title="""<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>"""
+html_part_after_title_before_code_block="""</title>
+        <style type="text/css">
+          .end-element { fill : #FFCCFF; }
+        </style>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.3.0/raphael.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowchart/1.17.1/flowchart.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.1/js/bootstrap.bundle.min.js"></script>
+        <!-- <script src="../release/flowchart.min.js"></script> -->
+        <script>
+
+            window.onload = function () {
+                var btn = document.getElementById("run"),
+                    cd = document.getElementById("code"),
+                    chart;
+
+                (btn.onclick = function () {
+                    var code = cd.value;
+
+                    if (chart) {
+                      chart.clean();
+                    }
+
+                    chart = flowchart.parse(code);
+                    chart.drawSVG('canvas', {
+                      'x': 0,
+                      'y': 0,
+                      'line-width': 3,
+                      //'maxWidth': 15,//ensures the flowcharts fits within a certain width
+                      'line-length': 50,
+                      'text-margin': 10,
+                      'font-size': 14,
+                      'font': 'normal',
+                      'font-family': 'Helvetica',
+                      'font-weight': 'normal',
+                      'font-color': 'black',
+                      'line-color': 'black',
+                      'element-color': 'black',
+                      'fill': 'white',
+                      'yes-text': 'yes',
+                      'no-text': 'no',
+                      'arrow-end': 'block',
+                      'scale': 1,
+                      'symbols': {
+                        'start': {
+						  'font-size': 14,
+                          'font-color': 'yellow',
+                          'element-color': 'blue',
+                          'fill': 'green',
+						  'class': 'start-element'
+                        },
+                        'inputoutput': {
+                          'font-color': 'black',
+                          'element-color': 'black',
+                          'fill': 'bisque'
+                        },
+                        'operation': {
+                          'font-color': 'black',
+                          'element-color': 'black',
+                          'fill': 'linen'
+                        },
+                        'subroutine': {
+                          'font-color': 'black',
+                          'element-color': 'blue',
+                          'fill': 'lightgreen'
+                        },
+                        'condition': {
+                          'font-color': 'red',
+						  'element-color': 'black',
+                          'fill': 'yellow'
+                        },
+                        'end':{
+						  'font-size': 20,
+                          'class': 'end-element'
+                        }
+                      },
+                      'flowstate' : {
+                        //'past' : { 'fill' : '#CCCCCC', 'font-size' : 12},
+                        //'current' : {'fill' : 'yellow', 'font-color' : 'red', 'font-weight' : 'bold'},
+                        //'future' : { 'fill' : '#FFFF99'},
+                        'request' : { 'fill' : 'blue'},
+                        'invalid': {'fill' : '#444444'},
+                        'approved' : { 'fill' : '#58C4A3', 'font-size' : 12, 'yes-text' : 'APPROVED', 'no-text' : 'n/a' },
+                        'rejected' : { 'fill' : '#C45879', 'font-size' : 12, 'yes-text' : 'n/a', 'no-text' : 'REJECTED' }
+                      }
+                    });
+
+                })();
+
+            };
+            function myFunction(event, node) {
+              console.log("You just clicked this node:", node);
+            }
+            
+        </script>
+		<script>
+			function HelpText() {
+			  var x = document.getElementById("HelpTextBlock");
+			  if (x.style.display === "none") {
+				x.style.display = "block";
+			  } else {
+				x.style.display = "none";
+			  }
+			}
+		</script>
+    </head>
+    <body>
+        <div><textarea id="code" style="width: 100%;" rows="11">"""
+
+html_part_after_code_block_remaining_html="""</textarea></div>
+        <div><button id="run" type="button">Run</button> <button onclick="HelpText()">Format Help</button></div>
+        
+		<div id="HelpTextBlock" style="display:none">Conditions can also be redirected like cond(yes, bottom) or cond(yes, right)
+... and the other symbols too... like sub1(right)<br/>
+You can also tweak the diagram.drawSVG('diagram', {}); script in this file for more changes<br/>
+This is based on <a href="https://github.com/adrai/flowchart.js">flowchart.js on github</a> and <a href="http://flowchart.js.org">http://flowchart.js.org</a> more documentation can be found over there.
+</div>
+
+        <div id="canvas"></div>
+    </body>
+</html>"""

commit 791790772890cb30cc80cefd648741adce809fd3

Moved output_html into output_html.py. Fixed typo. Moved the majority of output_html logic into output_html.py leaving only enough in __main__.py to pass along what output_html needs if output is specified. Fixed typo in print(f"Saved HTML to {output_name}") introduced in commit d78439bf4dfaa5555398c3217217b6ed281b7753

pyflowchart/__main__.py
@@ -11,9 +11,7 @@ import argparse
 import chardet
 
 from pyflowchart.flowchart import Flowchart
-
-from pyflowchart.output_html import html_part_before_title,html_part_after_title_before_code_block,html_part_after_code_block_remaining_html
-import os
+from pyflowchart.output_html import output_html
 
 def detect_decode(file_content: bytes) -> str:
     """detect_decode detect the encoding of file_content,
@@ -64,26 +62,10 @@ def main(code_file, field, inner, output, simplify, conds_align):
                                     simplify=simplify,
                                     conds_align=conds_align)
     print(flowchart.flowchart())
-    #If output file was specified, 
-    #Write output html format
-    #html blocks stored as variables in output_html.py
-    #html_part_before_title is header before <title>, 
-    #If field specified use that as title. If not, use output filename as title.
-    #Output remainder of header up to the code block (html_part_after_title_before_code_block)
-    #Output the flowchart.flowchart() into the code block section of the html
-    #Output the remainder of the html. (html_part_after_code_block_remaining_html)
-    #End code block. Add couple buttons. Add output canvas.
+    
+    # if output file was specified, send output, field, and flowchart over to output_html function
     if bool(output):
-        with open(output, 'w') as f:
-            f.write(html_part_before_title)
-            if bool(field):
-                f.write(field)
-            else:
-                f.write(os.path.basename(f.name))
-            f.write(html_part_after_title_before_code_block)
-            f.write(flowchart.flowchart())
-            f.write(html_part_after_code_block_remaining_html)
-            print(f"Saved HTML togit co{output}")
+        output_html(output_name=output, field_name=field, flowchart=flowchart.flowchart())
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Python code to flowchart.')

pyflowchart/output_html.py
@@ -12,13 +12,15 @@ Rest header into body html until you get to code block
 <insert code block>
 Remainder body of html closing up the code block, adding couple buttons, and canvas generated by code block.
 """
+def output_html(output_name: str, field_name: str, flowchart: str) -> None:
 
-html_part_before_title="""<!DOCTYPE html>
+    # Three raw html code sections encapsulated in triple quotes """
+    html_part_before_title="""<!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="utf-8">
         <title>"""
-html_part_after_title_before_code_block="""</title>
+    html_part_after_title_before_code_block="""</title>
         <style type="text/css">
           .end-element { fill : #FFCCFF; }
         </style>
@@ -126,7 +128,7 @@ html_part_after_title_before_code_block="""</title>
     <body>
         <div><textarea id="code" style="width: 100%;" rows="11">"""
 
-html_part_after_code_block_remaining_html="""</textarea></div>
+    html_part_after_code_block_remaining_html="""</textarea></div>
         <div><button id="run" type="button">Run</button> <button onclick="HelpText()">Format Help</button></div>
         
 		<div id="HelpTextBlock" style="display:none">Conditions can also be redirected like cond(yes, bottom) or cond(yes, right)
@@ -138,3 +140,15 @@ This is based on <a href="https://github.com/adrai/flowchart.js">flowchart.js on
         <div id="canvas"></div>
     </body>
 </html>"""
+    # No checking or prompting if output file already exists.
+    # This will overwrite if given an existing output file
+    with open(output_name, 'w') as f:
+            f.write(html_part_before_title)
+            if bool(field_name):
+                f.write(field_name)
+            else:
+                f.write(os.path.basename(f.name))
+            f.write(html_part_after_title_before_code_block)
+            f.write(flowchart)
+            f.write(html_part_after_code_block_remaining_html)
+            print(f"Saved HTML to {output_name}")