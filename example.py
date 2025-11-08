d79208c00e59daeb34ce1a08d4d57a5a2871b857
Richard Song <48084418+rich-song@users.noreply.github.com>
2025-11-07 17:42:03 -0800

    chore: ran black

--- example.py
+++ example.py
@@ -9,7 +9,7 @@ license that can be found in the LICENSE file.
 import json
 from pyreactflow import ReactFlow
 
-code = '''
+code = """
 def main() -> list[str]:
     customer_ids = get_customer_ids()
     results = []
@@ -20,13 +20,15 @@ def main() -> list[str]:
     else:
         print("no need for processing since there is no customer")
     return results
-'''
+"""
+
 
 # Generate the react-flow compatible dict from code
 def main():
     rf = ReactFlow.from_code(code, field="main", simplify=False, inner=False)
     react_flow = rf.export()
     print(json.dumps(react_flow, indent=2))
+
 
 if __name__ == "__main__":
     main()
